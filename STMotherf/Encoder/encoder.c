c522d32dde4ad6e0438002f931ca69867a2948b4
KhoaHo <1khoaho@gmail.com>
2023-10-27 14:42:56 +0700

    replace OOP to struct

--- STMotherf/Encoder/encoder.c
+++ STMotherf/Encoder/encoder.c
@@ -1,28 +1,28 @@
 #include "encoder.h"
 #define PULSE 98
 #define MAX_LENGTH 65535
-void Encoder::Encoder_Init()
+void Encoder_Init(Encoder *left, Encoder *right)
 {
     // HAL_TIM_Encoder_Init(htim, sConfig);
-    // check parameters
-    if (HAL_TIM_Encoder_Start(htim, TIM_CHANNEL_ALL) != HAL_OK)
-        Error_Handler(); // write in main.c, maybe turn some led on?
-    //
+    TIM_HandleTypeDef htim3;
+    TIM_HandleTypeDef htim4;
 
-    // notice that we using 2 phase encoder
-    HAL_TIM_Encoder_Start_DMA(htim, TIM_CHANNEL_ALL, value_1, value_2, 16);
+    if (HAL_TIM_Encoder_Start_DMA(htim3, TIM_CHANNEL_ALL, value_1, value_2, 16) != HAL_OK)
+        Error_Handler(); // write in main.c, maybe turn some led on?
+    if (HAL_TIM_Encoder_Start_DMA(htim4, TIM_CHANNEL_ALL, value_1, value_2, 16) != HAL_OK)
+        Error_Handler();
 }
 
-int Encoder::RPM()
+int RPM(TIM_HandleTypeDef *htim, Encoder *p)
 {
-    uint32_t prev_count = value_1;
-    uint16_t prev_time = htim->->Instance->CNT;
+    uint32_t prev_count = p->value_1;
+    uint16_t prev_time = htim->Instance->CNT;
     asm("");
-    int rpm = (((value_1 - prev_count + MAX_LENGTH*2) % (MAX_LENGTH*2))/PULSE)*60 / (((htim->->Instance->CNT - prev_time + MAX_LENGTH)/MAX_LENGTH)/16e6);
+    int rpm = (((p->value_1 - prev_count + MAX_LENGTH*2) % (MAX_LENGTH*2))/PULSE)*60 / (((htim->Instance->CNT - prev_time + MAX_LENGTH)/MAX_LENGTH)/16e6);
     return rpm;
 }
 
-byte Encoder::Direction()
+byte Direction(Encoder *p)
 {
-    return (byte)(value_1 - value_2);
+    return (p->value_1-p->value_2)%2;
 }
--- STMotherf/Encoder/encoder.h
+++ STMotherf/Encoder/encoder.h
@@ -1,17 +1,33 @@
 #ifndef encoder_h
 #define encoder_h
 
-class Encoder 
+typedef struct 
 {
-private:
     uint32_t value_1, value_2;
     TIM_HandleTypeDef *htim;
-public: 
-    Encoder(TIM_HandleTypeDef *htim):htim(htim) {};
-    void Encoder_Init();
-    int RPM(); //return speed of encoder
-    byte Direction(); //get direction
-}encoder_1, encoder_2;
+}Encoder;
 
+/**
+  * @brief  Initializes the encoder in DMA mode
+  * @param  left The destination Buffer address for encoder left
+  * @param  right The destination Buffer address for encoder right
+*/
+void Encoder_Init(Encoder *left, Encoder *right);
+
+/**
+  * @brief  Get value of encoder in rpm
+  * @param  htim Timer handle of encoder module
+  *          This parameter can be one of the following values:
+  *            @arg htm3: Timer handle 3, can shorten when known fixed pins
+  *            @arg htm4: Timer handle 4, can shorten when known fixed pins
+  * @param  p pointer to the encoder structure
+*/
+int RPM(TIM_HandleTypeDef *htim, Encoder *p); 
+
+/**
+  * @brief  Get direction of motor
+  * @param  p pointer to the encoder structure
+*/
+byte Direction(Encoder *p);
 
 #endif
