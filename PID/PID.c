88833bf0385b10f370471a22ab24246e015fef47
dang-khoa-2k4 <khoa.nguyentrandang@hcmut.edu.vn>
2023-11-22 20:57:17 +0700

    Update PID.c

--- PID/PID.c
+++ PID/PID.c
@@ -13,6 +13,7 @@ void reset_PID_gain(PID_instance *PID)
     PID->p_gain = 0.0;
     PID->i_gain = 0.0;
     PID->d_gain = 0.0;
+    PID->pre_error = 0;
     PID->isSaturation = false;
     return;
 }
@@ -46,9 +47,15 @@ void output_PID(PID_instance *PID, int16_t error_input, uint16_t sampling_rate)
                     + PID->d_gain * (error_input - PID->pre_error) * sampling_rate;
 
     int16_t raw_value = PID->output_PID;
+<<<<<<< Updated upstream
     constrain(&(PID->output_PID), -MAX_PID_VALUE, MAX_PID_VALUE);
     
     PID->isSaturation = (raw_value != PID->output_PID) && (raw_value * error_input > 0); //check saturation and same sign
+=======
+    constrain(PID->output_PID, -MAX_PID_VALUE, MAX_PID_VALUE);
+    //check saturation and same sign
+    PID->isSaturation = (raw_value != PID->output_PID) && (raw_value * error_input > 0); 
+>>>>>>> Stashed changes
     PID->pre_error = error_input;
     return;
 }
