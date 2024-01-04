7c7f520d109cbc2f1d78394b36793eb74dc823cb
dang-khoa-2k4 <khoa.nguyentrandang@hcmut.edu.vn>
2024-01-04 18:02:32 +0700

    update

--- UltraSonic/UltraSonic.h
+++ UltraSonic/UltraSonic.h
@@ -1,22 +1,29 @@
+/*
+  ***Warnming***
+      Each timer is only used for one ultra sonic sensor
+      And channel used for your sensor is always TIM_CHANNEL_1
+*/
+
+
 #ifndef _ULTRA_SONIC_
 #define _ULTRA_SONIC_
 
-#include "..\util\DWT_Delay.h"
 #include "..\AVERAGE FILTER\average_filter.h"
+#include <stdbool.h>
 
-#define SOUND_SPEED 34320
-#define STEP_TIMER1 4e-6 // prescalers / SysClk = 32 / 8Mhz
-#define TIMER1_STEP_CYCLE (1 << 16)-1 // 65526
+#define SOUND_SPEED 34320 // cm/s
+#define STEP_TIMER 1e-6 // prescalers / SysClk = 72 / 72Mhz = 1 us
+#define TIMER_STEP_CYCLE (1 << 16)-1 // 65536
 #define SAMPLING_TIME 25 // ms
 
 typedef struct
 {
     TIM_HandleTypeDef *htim;
-    uint16_t pre_time;
-    uint16_t distance; // in cm
+    volatile uint16_t pre_time;
+    volatile double distance; // in cm
     GPIO_TypeDef *TrigPort;
-    uint16_t TrigPin;
-    boolean echoHigh;
+    uint32_t TrigPin;
+    bool echoHigh;
     average_filter *filter;
 } ultraSonic;
 
@@ -26,7 +33,7 @@ typedef struct
   * @param  *htim is timer used for edge detect echo pin
   * @param  *TrigPort, TrigPin is port and pin of trig pin
 */
-void ultraSonic_Init(ultraSonic *sensor, TIM_HandleTypeDef *htim, GPIO_TypeDef *TrigPort, uint16_t TrigPin);
+void ultraSonic_Init(ultraSonic *sensor, TIM_HandleTypeDef *htim, average_filter *filter, GPIO_TypeDef *TrigPort, uint16_t TrigPin);
 
 /**
   * @brief  update all paramater in ultra sonic structure
@@ -35,9 +42,15 @@ void ultraSonic_Init(ultraSonic *sensor, TIM_HandleTypeDef *htim, GPIO_TypeDef *
 void updateDistance(ultraSonic *sensor);
 
 /**
-  * @brief  set trig pin
+  * @brief  set trig pin and active Interrupt
   * @param  *sensor is pointer to the ultra sonic structure
 */
 void activeTrigger(ultraSonic *sensor);
 
+/**
+  * @brief delay is used for set trig pin in us
+  * @param  *htim is timer used for edge detect echo pin
+  * @param  time is the time you need to delay
+**/
+void delay_us(TIM_HandleTypeDef *htim, uint16_t time);
 #endif
--- UltraSonic/ultraSonic.c
+++ UltraSonic/ultraSonic.c
@@ -1,6 +1,6 @@
 #include "ultraSonic.h"
 
-void ultraSonic_Init(ultraSonic *sensor, TIM_HandleTypeDef *htim, GPIO_TypeDef *TrigPort, uint16_t TrigPin)
+void ultraSonic_Init(ultraSonic *sensor, TIM_HandleTypeDef *htim, average_filter *filter, GPIO_TypeDef *TrigPort, uint16_t TrigPin)
 {
     sensor->htim = htim;
     sensor->pre_time = 0;
@@ -8,23 +8,41 @@ void ultraSonic_Init(ultraSonic *sensor, TIM_HandleTypeDef *htim, GPIO_TypeDef *
     sensor->TrigPort = TrigPort;
     sensor->TrigPin = TrigPin;
     sensor->echoHigh = false;
-    DWT_Delay_Init();
+    sensor->filter = filter;
+    HAL_GPIO_WritePin(sensor->TrigPort, sensor->TrigPin, 0);
     reset_buffer(sensor->filter);
 }
 
 // throw into void HAL_TIM_IC_CaptureCallback (TIM_HandleTypeDef* htim)
 void updateDistance(ultraSonic *sensor)
 {
-    if (sensor->echo == true)
+    if (sensor->htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1)
     {
-        sensor->pre_time = sensor->htim->Instance->CNT; // ? or CCR ?
+        if (sensor->echoHigh == false)
+        {
+            sensor->pre_time = HAL_TIM_ReadCapturedValue(sensor->htim, TIM_CHANNEL_1);
+//            __HAL_TIM_SET_CAPTUREPOLARITY(sensor->htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_FALLING);
+            sensor->echoHigh = true;
+            __HAL_TIM_DISABLE_IT(sensor->htim, TIM_IT_CC1);
+            __HAL_TIM_ENABLE_IT(sensor->htim, TIM_IT_CC2);
+        }
     }
-    else 
-    {   
-        apply_filter(sensor->filter, sensor->htim->Instance->CNT - sensor->pre_time);
-        sensor->distance = ((SOUND_SPEED * STEP_TIMER1) * (sensor->filter->out)) / 2; 
+    else if (sensor->htim->Channel == HAL_TIM_ACTIVE_CHANNEL_2)
+    {
+    	if (sensor->echoHigh == true)
+    	        {
+    	//        	uint32_t diffTime = (sensor->htim->Instance->CCR1 - sensor->pre_time + TIMER_STEP_CYCLE) % TIMER_STEP_CYCLE;
+    	        	uint32_t diffTime = (HAL_TIM_ReadCapturedValue(sensor->htim, TIM_CHANNEL_2) - sensor->pre_time + TIMER_STEP_CYCLE) % TIMER_STEP_CYCLE;
+
+    	            apply_filter(sensor->filter, diffTime);
+    	            sensor->distance = ((SOUND_SPEED * STEP_TIMER) * (sensor->filter->out)) / 2;
+    	            sensor->echoHigh = false;
+    	            sensor->htim->Instance->CNT = 0;
+
+//    	            __HAL_TIM_SET_CAPTUREPOLARITY(sensor->htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_RISING);
+    	            __HAL_TIM_DISABLE_IT(sensor->htim, TIM_IT_CC2);
+    	        }
     }
-    sensor->echoHigh = !sensor->echoHigh;
 }
 
 // throw into void SysTick_CallBack(void)
@@ -42,7 +60,15 @@ void updateDistance(ultraSonic *sensor)
 void activeTrigger(ultraSonic *sensor)
 {
     HAL_GPIO_WritePin(sensor->TrigPort, sensor->TrigPin, 1);
-    DWT_Delay_us(10);
+    delay_us(sensor->htim, 10);
     HAL_GPIO_WritePin(sensor->TrigPort, sensor->TrigPin, 0);
+    __HAL_TIM_ENABLE_IT(sensor->htim, TIM_IT_CC1);
+//    __HAL_TIM_ENABLE_IT(sensor->htim, TIM_IT_CC2);
+}
+
+void delay_us(TIM_HandleTypeDef *htim, uint16_t time)
+{
+    htim->Instance->CNT = 0; // __HAL_TIM_SET_COUNTER(&htim, Cnt);
+    while (htim->Instance->CNT < time);  // while (__HAL_TIM_GET_COUNTER(&htim) < time)
 }
 
